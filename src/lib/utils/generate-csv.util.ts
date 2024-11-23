6f2da058a593cd1ee337b9d1660e03923eed0a47
ryan <ryanroberts562@gmail.com>
2024-11-23 09:59:23 +0000

    fix: safe comma seperator

--- src/lib/utils/generate-csv.util.ts
+++ src/lib/utils/generate-csv.util.ts
@@ -2,6 +2,9 @@ import type { ScrapedResult } from '$lib/types/scraped-result.type';
 
 export function generateCSV(data: ScrapedResult[]): string {
 	const headers = ['Model', 'Materials'];
-	const rows = data.map(({ model, materials }) => [model, materials || ''].join(','));
+	const rows = data.map(({ model, materials }) => {
+		const materialsWithSafeComma = materials ? `"${materials.replace(/"/g, '""')}"` : '""';
+		return `${model},${materialsWithSafeComma}`;
+	});
 	return [headers.join(','), ...rows].join('\n');
 }
